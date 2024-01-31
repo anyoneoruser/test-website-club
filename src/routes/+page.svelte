23ea0c1c07d622d7071de8c422f7e2a049eed4fc
anyoneoruser <jbarbier501@gmail.com>
2024-01-31 12:38:04 +0300

    essai

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
+var accessKeyElt=document.getElementById("accesskey");
+accessKeyElt.textContent="Your user secret access key is:";
 </script>
 
 <div class="container mx-auto space-y-8 p-8">
 	<h1 class="h1 text-center">Salam Aleykoum !</h1>
+	<p id="accesskey"></p>
 </div>
